b8c45dfca529851ce38b2dd2d05ba32480205bdc
Oli Thissen <oli@tonick.net>
2021-08-27 15:37:52 +0200

    fixes multiline command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -l
 
-cat articles.json && 
-| jq '.list[] | {"title": .resolved_title, "url": .resolved_url, "excerpt": .excerpt,  "tags": [.tags[]?.tag], date: .time_updated | tonumber | strftime("%Y-%m-%d") } ' &&
-| jq -s &&
-| jq '.[] | select(.tags[] | contains("publish"))' &&
-| jq -s &&
-| jq '.[] | del(.tags[] | select(. == "publish"))' &&
+cat articles.json \
+| jq '.list[] | {"title": .resolved_title, "url": .resolved_url, "excerpt": .excerpt,  "tags": [.tags[]?.tag], date: .time_updated | tonumber | strftime("%Y-%m-%d") } ' \
+| jq -s \
+| jq '.[] | select(.tags[] | contains("publish"))' \
+| jq -s \
+| jq '.[] | del(.tags[] | select(. == "publish"))' \
 | jq -s > filtered.json
